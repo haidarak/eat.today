c3de93e9e606f48941964e6c2ed9cd2b17099f27
Haidar Aji Kalbuadi <haidar.aji.kalbuadi@gmail.com>
2017-06-18 15:24:44 +0700

    Update food database

--- randomizer.js
+++ randomizer.js
@@ -4,13 +4,13 @@ var foodArray =
 'Cakalang', 'Cap cai', 'Daun ubi tumbuk', 'Empal gentong', 'Empal gepuk', 'Gado-gado', 'Gudeg', 
 'Gulai', 'Iga penyet', 'Ikan asin', 'Ikan bakar', 'Ikan goreng', 'Indomie', 'Karedok', 'Ketoprak', 
 'Ketupat sayur', 'Krechek', 'Krengsengan', 'Kwetiau ayam', 'Kwetiau goreng', 'Laksa Banjar', 
-'Laksa Bogor', 'Laksa Jakarta', 'Lawar', 'Lontong Cap Go Meh', 'Lontong sayur', 'Mie aceh', 
-'Mie ayam', 'Mie celor', 'Mie goreng', 'Mie kangkung', 'Mie kocok', 'Mie koclok', 'Mie rebus', 
+'Laksa Bogor', 'Laksa Jakarta', 'Lawar', 'Lontong Cap Go Meh', 'Lontong sayur', 'Martabak Manis', 'Martabak', 
+'Mie aceh', 'Mie ayam', 'Mie celor', 'Mie goreng', 'Mie kangkung', 'Mie kocok', 'Mie koclok', 'Mie rebus', 
 'Mie yamin', 'Nasi campur (Nasi Rames)', 'Nasi goreng', 'Nasi kuning', 'Nasi liwet', 'Nasi Padang', 
 'Nasi pecel', 'Nasi tim', 'Nasi uduk', 'Nasi ulam', 'Oncom', 'Opor Ayam', 'Pallubasa', 'Paniki', 
 'Papeda', 'Pecel Lele', 'Pempek', 'Pepes', 'Perkedel Jagung', 'Plecing kangkung', 'Rawon', 'Rendang', 
 'Rica-rica', 'Roti Chanai', 'Saksang', 'Sambal goreng teri', 'Sate Ayam', 'Sate Kambing', 'Sate lilit', 
-'Sayur asem', 'Sayur lodeh', 'Sayur Nganten', 'Seblak', 'Steak', 'Sushi', 'Selat solo', 'Semur', 'Sop buntut', 'Sop', 
+'Sayur asem', 'Sayur lodeh', 'Sayur Nganten', 'Sayur Nangka', 'Seblak', 'Steak', 'Sushi', 'Selat solo', 'Semur', 'Sop buntut', 'Sop', 
 'Soto Ayam', 'Soto Daging', 'Sroto', 'Coto', 'Tekwan', 'Tauge goreng', 'Telur pindang', 'Tempe', 
 'Tempoyak ikan patin', 'Tinutuan or Bubur Manado', 'Tongseng', 'Tumis Kangkung', 'Tumpang', 'Tumpeng', 'Woku'];
 
