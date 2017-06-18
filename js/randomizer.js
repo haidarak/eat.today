a7cdbaf1ed0e0365c8b7a147a48dec54e78e4de3
Haidar Aji Kalbuadi <haidar.aji.kalbuadi@gmail.com>
2017-06-18 18:59:23 +0700

    Add food in data pool

--- js/randomizer.js
+++ js/randomizer.js
@@ -3,16 +3,19 @@ var foodArray =
 'Bakwan Malang', 'Bakso', 'Bebek goreng', 'Betutu', 'Bubur ayam', 'Bubur Manado (Tinutuan)', 
 'Cakalang', 'Cap cai', 'Daun ubi tumbuk', 'Empal gentong', 'Empal gepuk', 'Gado-gado', 'Gudeg', 
 'Gulai', 'Iga penyet', 'Ikan asin', 'Ikan bakar', 'Ikan goreng', 'Indomie', 'Karedok', 'Ketoprak', 
-'Ketupat sayur', 'Krechek', 'Krengsengan', 'Kwetiau ayam', 'Kwetiau goreng', 'Laksa Banjar', 
+'Ketupat sayur', 'Krechek', 'Krengsengan', 'Kwetiau ayam', 'Kwetiau goreng', 'Kare', 'Laksa Banjar', 
 'Laksa Bogor', 'Laksa Jakarta', 'Lawar', 'Lontong Cap Go Meh', 'Lontong sayur', 'Martabak Manis', 'Martabak', 
-'Mie aceh', 'Mie ayam', 'Mie celor', 'Mie goreng', 'Mie kangkung', 'Mie kocok', 'Mie koclok', 'Mie rebus', 
+'Mie aceh', 'Mie ayam', 'Mie celor', 'Mie goreng', 'Mie kocok', 'Mie koclok', 'Mie rebus', 
 'Mie yamin', 'Nasi campur (Nasi Rames)', 'Nasi goreng', 'Nasi kuning', 'Nasi liwet', 'Nasi Padang', 
 'Nasi pecel', 'Nasi tim', 'Nasi uduk', 'Nasi ulam', 'Oncom', 'Opor Ayam', 'Pallubasa', 'Paniki', 
 'Papeda', 'Pecel Lele', 'Pempek', 'Pepes', 'Perkedel Jagung', 'Plecing kangkung', 'Rawon', 'Rendang', 
 'Rica-rica', 'Roti Chanai', 'Saksang', 'Sambal goreng teri', 'Sate Ayam', 'Sate Kambing', 'Sate lilit', 
 'Sayur asem', 'Sayur lodeh', 'Sayur Nganten', 'Sayur Nangka', 'Seblak', 'Steak', 'Sushi', 'Selat solo', 'Semur', 'Sop buntut', 'Sop', 
 'Soto Ayam', 'Soto Daging', 'Sroto', 'Coto', 'Tekwan', 'Tauge goreng', 'Telur pindang', 'Tempe', 
-'Tempoyak ikan patin', 'Tinutuan or Bubur Manado', 'Tongseng', 'Tumis Kangkung', 'Tumpang', 'Tumpeng', 'Woku'];
+'Tempoyak ikan patin', 'Tinutuan or Bubur Manado', 'Tongseng', 'Tumis Kangkung', 'Tumpang', 'Tumpeng', 'Woku',
+'Jamur','Pizza', 'Pasta', 'Cake', 'Roti', 'Tahu', 'Burger', 'Iga Bakar', 'Dimsum', 'Udang Goreng', 'Cumi Goreng',
+'Seafood Asam Manis', 'Bebek Bakar', 'Sashimi', 'Udon', 'Ramen', 'Mozaru', 'Yakitori', 'Donburi', 'Oyakodon',
+'I fu Mie', 'Yakiniku', 'Shabu Shabu', 'Taco', 'Quesadillas', 'Teriyaki'];
 
 function generateRandomFood(){
 	var foodString = foodArray[Math.floor(Math.random() * foodArray.length)];
